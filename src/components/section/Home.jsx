da359c83db5a9a4618b90b177569a67b4b16d5ea
shreyya18 <shreyapoojary251@gmail.com>
2025-06-01 18:56:48 +0530

    contact done

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -8,11 +8,11 @@ export const Home = () => {
             text-transparent leading-right
             ">Hi, I'm Shreya</h1>
             <p className="text-gray-400 text-lg mb-8  max-w-lg mx-auto">
-                I'm a dedicated individual specializing in web development and problem-solving, with a strong commitment to
+                A dedicated individual specializing in web development and problem-solving, with a strong commitment to
                 continuous learning and team collaboration.
             </p>
             <div className="flex justify-center space-x-4">
-                <a href="#projects" className="bg-blue-500  text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.4)] ">
+                <a href="#project" className="bg-blue-500  text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.4)] ">
                     View Projects
                 </a>
                 <a href="#contact" className=" border border-blue- 500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.2)] hover:bg-blue-500/10 ">

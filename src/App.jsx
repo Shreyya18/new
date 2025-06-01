5a9debb20dc4da27b4390ec5867a833e972bca2d
shreyya18 <shreyapoojary251@gmail.com>
2025-06-01 18:56:40 +0530

    loading screen done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from './components/section/Home';
 import { About } from './components/section/About';
 import { Project } from './components/section/Project';
 import './index.css';
+import { Contact } from './components/section/Contact';
 function App() {
   const[isLoaded,setIsLoaded]=useState(false);
   const[menuOpen,setMenuOpen]=useState(false);
@@ -20,6 +21,7 @@ function App() {
         <Home/>
         <About/>
         <Project/>
+        <Contact/>
       </div>
     </>
   )

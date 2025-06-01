a9733f01528671b82b82dd434c31000c170b7b03
shreyya18 <shreyapoojary251@gmail.com>
2025-06-01 20:53:29 +0530

    center align

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
     return <section id="about" className="min-w-max min-h-screen flex items-center justify-center py-20 m-0 ">
         <RevealOnScroll>
         <div className=" max-w-3xl max-auto px-4 text-xl md:text-2xl">
-            <h2 className=" text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 0 bg-clip-text text-transparent">About Me</h2>
+            <h2 className=" text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 0 bg-clip-text text-transparent flex justify-center">About Me</h2>
             <div className=" rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
                 <p className="text-gray-300 mb-6">
                     Adaptable web developer driven by growth, creativity, and problem-solving.

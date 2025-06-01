f89a2df0fe5e428d8d4fd138a4147965da406674
shreyya18 <shreyapoojary251@gmail.com>
2025-06-01 20:47:44 +0530

    md-1

--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -1,9 +1,9 @@
 import { RevealOnScroll } from "../RevealOnScroll"
 
 export const Project = () => {
-    return <section id="project" className="min-h-screen flex item-center justify-center py-20">
+    return <section id="project" className="min-w-max min-h-screen flex item-center justify-center py-20">
         <RevealOnScroll>
-        <div className="max-w-5xl mx-auto px-4">
+        <div className="max-w-3xl mx-auto px-4 text-xl md:text-2xl mr-20 ml-20 ">
             <h2 className="flex justify-center text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 0 bg-clip-text text-transparent">Featured Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-xl">
                 <div className="p-6 rounded-xl border-white/10 hover:translate-y-1 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">

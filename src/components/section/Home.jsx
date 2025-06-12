e11b06a4f7e199924592aebf35af1e56bff67665
shreyya18 <shreyapoojary251@gmail.com>
2025-06-12 22:23:19 +0530

    movile view fix 3

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -49,7 +49,7 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="w-full min-h-screen flex items-center justify-center px-4 py-10 md:py-20 md:px-0 overflow-x-hidden"
+      className="w-screen h-screen flex items-center justify-center px-4 py-10 md:py-20 md:px-0 overflow-hidden"
     >
       <Helmet>
         <title>Shreya poojary: Portfolio</title>
@@ -58,7 +58,7 @@ export const Home = () => {
       </Helmet>
 
       <RevealOnScroll>
-        <div className="text-center z-10">
+        <div className="w-full max-w-screen-md mx-auto text-center">
           <h1 className="text-4xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-600 bg-clip-text text-transparent leading-tight">
             Hi, I'm Shreya
           </h1>
@@ -66,10 +66,10 @@ export const Home = () => {
             A dedicated individual specializing in web development and problem-solving,
             with a strong commitment to continuous learning and team collaboration.
           </p>
-          <div className="flex flex-col md:flex-row justify-center gap-4">
+          <div className="flex flex-col md:flex-row justify-center gap-4 w-full">
             <a
               href="#project"
-              className="bg-blue-500 text-white py-4 md:py-3 px-6 w-full md:w-auto mb-4 md:mb-0 rounded font-medium transition hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.4)] text-xl md:text-3xl"
+              className="bg-blue-500 text-white py-4 md:py-3 px-6 w-full md:w-auto rounded font-medium transition hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.4)] text-xl md:text-3xl"
             >
               View Projects
             </a>
